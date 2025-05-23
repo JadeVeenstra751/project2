da10c44e0555c8d5bce7c198301f3381a085c6c8
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 19:34:20 +1000

    adding css for login form

--- login.php
+++ login.php
@@ -1,16 +1,20 @@
 <?php include 'header.inc'; ?>
 <meta name="author" content="Jade Veenstra">
-<title>Login - LeafByte Tech</title>
+<title>LeafByte Tech Login Page</title>
 </head>
 
 <body>
 <?php include 'nav.inc'; ?>
 
 <form action="process.php" method="post">
-  Username: <input type="text" name="username"><br>
-  Password: <input type="password" name="password"><br>
-  <input type="submit" value="Login">
-  
-<?php include 'footer.inc'; ?>
-</body>
-</html>
+  <div class="login-form">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username">
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password">
+
+    <input type="submit" value="Login">
+  </div>
+
+</form>
