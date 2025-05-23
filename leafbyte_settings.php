6ab228c89e57ca62d98e1331a06075d0d06d1430
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 17:59:37 +1000

    testing to see if css will work again

--- leafbyte_settings.php
+++ leafbyte_settings.php
@@ -1,32 +1,30 @@
-    <?php include 'header.inc';?>
-    <!--author information-->
+<?php
+session_start();
+$isLoggedIn = isset($_SESSION['username']);
+$isManager = isset($_SESSION['role']) && $_SESSION['role'] === 'manager';
+
+include 'header.inc'; ?>
     <meta name="author" content="Jade Veenstra">
-    <!--title of page-->
-    <title>LeafByte Tech </title>
+    <title>Settings Page</title>
 </head>
-<!-- body is declared as home so it is unique to this html page-->
-<body class="home">
-    <!-- navigation menu with hyperlinks to the about, apply, index and job pages, along with the e-mail contact-->
-	<?php include 'nav.inc';?>
-    <!--title is given a separate id-->
-    <h1 id="leafbytetitle">LEAFBYTE TECH</h1>
-    <!-- the company information is sectioned off-->
-    <section id="indexcompanyinfo">
-        <p id="companydescription">LeafByte Tech is an accredited job advertising platform focused on the technology industry. Here at LeafByte, we assist businesses in finding the right candidates and connect job seekers with the perfect opportunities through reliable postings, aiming to link skilled technology professionals with innovative companies in various sectors.</p> 
-        <hr class="indexdivider1">
-        <p>From software engineering roles to IT support positions, there are various listings on offer.</p>
-        <p><a id="indexinfosearch" href="/project1/jobs.html"><strong>SEARCH NOW</strong></a></p>
-        <hr class="indexdivider2">
-    <!-- text written by Moji, implemented into Jade's code-->
-        <p>If you are ready to make your mark within the tech world, join us today in our journey to transform the industry, one byte at a time.</p>
-        <p><a id="indexinfoapply" href="/project1/apply.html"><strong>APPLY NOW</strong></a></p>
-        <hr class="indexdivider3">
-        <p id="emailink">Or contact us at <a href="mailto:info@LeafByteTech.com.au">info@LeafByteTech.com.au</a> for any inquiries.</p>
-    </section>
-    <!-- image of the company logo. logo was created on canva.com-->
-    <img id="imagelogo" src="images/leafbytetechlogo.png" alt="CompanyLogo">
-    <!-- line breaks so the footer doesnt cover text (dont know the problem?)-->
-    <footer class="footer">
-    <?php include 'footer.inc';?>
+
+<body>
+<?php include 'nav.inc';
+
+if (!$isLoggedIn) {
+    echo '<section id="indexcompanyinfo">';
+    echo '<p><a id="indexinfoapply" href="/login.php"><strong>APPLY NOW</strong></a></p>';
+    echo '</div>';
+} else {
+    echo '<p>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</p>';
+    echo '<p class="leafbytebuttons"><a href="./logout.php" class="CustomLink">Logout</a></p>';
+    if ($isManager) {
+        echo '<p class="leafbytebuttons"><a href="./manage.php" class="CustomLink">Manage</a></p>';
+    }
+}
+
+include 'footer.inc';
+?>
+
 </body>
 </html>
