912650c6c4f59ad649c741c885c7c68a46f4c554
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 21:11:31 +1000

    adding styling

--- enhancements.php
+++ enhancements.php
@@ -28,12 +28,13 @@ if (!$isLoggedIn) {//if user not logged in then
     echo '<body style="background-color: #caba9c;">';
     echo '<div style="text-align:center; margin: 2em 0;">';
     echo '<a id="indexinfoapply" href="./login.php" class="CustomLink" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block;">Login</a>';
-    echo '</div>';
 } else {
-    echo '<p>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</p>';
+    echo '<h1>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</h1>';
     echo '<p class="leafbytebuttons"><a href="./logout_process.php" class="CustomLink">Logout</a></p>';
     if ($isManager) {
-        echo '<p class="leafbytebuttons"><a href="./manage.php" class="CustomLink">Manage</a></p>';
+        echo '<p class="leafbytebuttons"><a href="./manage.php" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block; class="CustomLink">Manage</a></p>';
+        echo '<p class="leafbytebuttons"><a href="./manage.php" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block; class="CustomLink">Manage</a></p>';
+    echo '</div>';
     }
 }
 
