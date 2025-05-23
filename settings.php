6af8d21902d0dd5fc8bfbbfcb7cb06f15f83c507
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 16:15:35 +1000

    removing the updated code

--- settings.php
+++ settings.php
@@ -1,15 +1,6 @@
 <?php
-$host = "localhost"; // server hostname
-$user = "root"; //username for SQL db
-$pwd = ""; //password for SQL user
-$sql_db = "job_db"; //name of db
-
-//connection to SQL database
-//$conn = mysqli_connect($host, $user, $pwd, $sql_db);
-
-//checks if connection is successful
-//if (!$conn) {
-    //if failed, display an error message and stop execution
-    //die("Connection failed: " . mysqli_connect_error());
-//}
+    $host = "localhost";
+    $user = "root";
+    $pwd = "";
+    $sql_db = "job_db";
 ?>
