581874f6661d738938adbdac017b8cb333a4992b
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 17:25:18 +1000

    giving leafbytesettings.php css

--- leafbyte_settings.php
+++ leafbyte_settings.php
@@ -7,12 +7,13 @@ include 'header.inc';
 include 'nav.inc';
 echo '<link rel="stylesheet" type="text/css" href="styles/styles.css">';
 if (!$isLoggedIn) {
-    echo '<p class="menu"><a href="./login.php" class="CustomLink">Login</a></p>';
+    echo '<div class="leafbytesettings">';
+    echo '<p class="leafbytebuttons"><a href="./login.php" class="CustomLink">Login</a></p>';
 } else {
     echo '<p>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</p>';
-    echo '<p class="menu"><a href="./logout.php" class="CustomLink">Logout</a></p>';
+    echo '<p class="leafbytebuttons"><a href="./logout.php" class="CustomLink">Logout</a></p>';
     if ($isManager) {
-        echo '<p class="menu"><a href="./manage.php" class="CustomLink">Manage</a></p>';
+        echo '<p class="leafbytebuttons"><a href="./manage.php" class="CustomLink">Manage</a></p>';
     }
 }
 
