118b648b3308b0aadcb9a560ddc5bbc2268c926e
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 20:42:11 +1000

    removing job db

--- settings.php
+++ settings.php
@@ -4,16 +4,16 @@ $user = "root";// username for SQL db
 $pwd = "";// password for SQL user
 
 // databases
-$sql_db1 = "job_db";
+//$sql_db1 = "job_db";
 $sql_db2 = "userprofile";
 
 // connect to job_db db
 //establish connection
-$conn1 = mysqli_connect($host, $user, $pwd, $sql_db1);
-if (!$conn1) {
+//$conn1 = mysqli_connect($host, $user, $pwd, $sql_db1);
+//if (!$conn1) {
     //if connection unsuccessful, stop execution
-    die("Connection to job_db failed: " . mysqli_connect_error());
-}
+    //die("Connection to job_db failed: " . mysqli_connect_error());
+//}
 
 // connect to userprofile db
 //establish connection
