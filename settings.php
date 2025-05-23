f010f36db025e610cc19506b231f3fd40ec87525
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 20:36:27 +1000

    changing settings php

--- settings.php
+++ settings.php
@@ -3,20 +3,23 @@ $host = "localhost"; //server hostname
 $user = "root";// username for SQL db
 $pwd = "";// password for SQL user
 
+// databases
+$sql_db1 = "job_db";
+$sql_db2 = "userprofile";
 
-$sql_db1 = "job_db";// name of db
+// connect to job_db db
 //establish connection
 $conn1 = mysqli_connect($host, $user, $pwd, $sql_db1);
-//if connection unsuccessful, stop execution
 if (!$conn1) {
-    die("Connection failed: " . mysqli_connect_error());
+    //if connection unsuccessful, stop execution
+    die("Connection to job_db failed: " . mysqli_connect_error());
 }
 
-$sql_db2 = "userprofile";// name of db
+// connect to userprofile db
 //establish connection
 $conn2 = mysqli_connect($host, $user, $pwd, $sql_db2);
-//if connection unsuccessful, stop execution
 if (!$conn2) {
-    die("Connection failed: " . mysqli_connect_error());
+    //if connection unsuccessful, stop execution
+    die("Connection to userprofile failed: " . mysqli_connect_error());
 }
 ?>
