1c5ddbcaa224d2c2e6a50edcea9f054cefc98252
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 21:02:57 +1000

    adding in session start

--- logout_process.php
+++ logout_process.php
@@ -1,10 +1,10 @@
 <?php
+session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
     // removes all session variables
     $_SESSION = array();
     // destroys the session
     session_destroy();
-
     // redirects to login page
     header("Location: login.php");
     exit();
