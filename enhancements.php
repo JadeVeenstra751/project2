b5aef96e2ddfbe3875b32002f5ad8475c32b8374
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 21:20:13 +1000

    changing the em to 4

--- enhancements.php
+++ enhancements.php
@@ -26,12 +26,12 @@ include 'header.inc'; ?>
 if (!$isLoggedIn) {//if user not logged in then
     //issues with css so i had to add in the css into the page to get it to work
     echo '<body style="background-color: #caba9c;">';
-    echo '<div style="text-align:center; margin: 2em 0;">';
+    echo '<div style="text-align:center; margin: 4em 0;">';
     echo '<a id="indexinfoapply" href="./login.php" class="CustomLink" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block;">Login</a>';
     echo '</div>';
 } else {
     echo '<body style="background-color: #caba9c;">';
-     echo '<div style="text-align:center; margin: 10em 0;">';
+     echo '<div style="text-align:center; margin: 2em 0;">';
     echo '<h1>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</h1>';
     echo '<a id="indexinfoapply" href="./logout_process.php" class="CustomLink">Logout</a>';
     if ($isManager) {
