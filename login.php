1da8b32b2ddd0e6655a551a1e9e908f71dfb6ab8
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 20:06:43 +1000

    changing login-form on css

--- login.php
+++ login.php
@@ -9,7 +9,6 @@
   <div class="login-form">
     <label for="username">Username:</label>
     <input type="text" name="username" id="username">
-    <br>
     <label for="password">Password:</label>
     <input type="password" name="password" id="password">
 
