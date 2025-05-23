1e30b98e06a5d2704b6ad2fad67cfd7bcc3d2941
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 21:04:20 +1000

    adding get to process

--- logout_process.php
+++ logout_process.php
@@ -7,6 +7,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
     session_destroy();
     // redirects to login page
     header("Location: login.php");
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // GET logout (link click)
+    $_SESSION = array();
+    session_destroy();
+    header("Location: login.php");
     exit();
 }
     ?>
