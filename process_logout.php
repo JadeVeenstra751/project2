307657fe48c75f8061308657275548b9b6765433
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 20:56:26 +1000

    adding a logout feature

---
+++ process_logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+// removes all session variables
+$_SESSION = array();
+// destroys the session
+session_destroy();
+// redirects to login page
+header("Location: ./login.php");
+exit();
+?>
