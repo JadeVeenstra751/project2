55d0f3f9b3e4c4fd06f4a38d585d5331354d80c0
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 20:04:55 +1000

    removing fieldset

--- login.php
+++ login.php
@@ -2,17 +2,21 @@
 <meta name="author" content="Jade Veenstra">
 <title>LeafByte Tech Login Page</title>
 <?php include 'nav.inc'; ?>
+</head>
 
+<body>
 <form action="process_login.php" method="post" background-color: #ab8462;>
   <div class="login-form">
-    <fieldset>
+
     <label for="username">Username:</label>
     <input type="text" name="username" id="username">
-    </fieldset>
-    <fieldset>
+
     <label for="password">Password:</label>
     <input type="password" name="password" id="password">
-  </fieldset>
+
     <input type="submit" value="Login">
   </div>
 </form>
+ <?php include 'footer.inc';?>
+</body>
+</html>
