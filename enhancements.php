7c4b21bb01351be5f678e3cf72e832d12de946d2
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 21:20:23 +1000

    to 8

--- enhancements.php
+++ enhancements.php
@@ -26,7 +26,7 @@ include 'header.inc'; ?>
 if (!$isLoggedIn) {//if user not logged in then
     //issues with css so i had to add in the css into the page to get it to work
     echo '<body style="background-color: #caba9c;">';
-    echo '<div style="text-align:center; margin: 4em 0;">';
+    echo '<div style="text-align:center; margin: 8em 0;">';
     echo '<a id="indexinfoapply" href="./login.php" class="CustomLink" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block;">Login</a>';
     echo '</div>';
 } else {
