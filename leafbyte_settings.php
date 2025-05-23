cb47981db11850a470051fe8364c56e5e63898c6
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 18:28:39 +1000

    making the button more round

--- leafbyte_settings.php
+++ leafbyte_settings.php
@@ -13,7 +13,7 @@ include 'header.inc'; ?>
 
 if (!$isLoggedIn) {
     echo '<div style="text-align:center; margin: 2em 0;">';
-    echo '<a id="indexinfoapply" href="/login.php" class="CustomLink" style="padding:1em 2em; font-size:1.5em; border-radius:20px; display:inline-block;">Login</a>';
+    echo '<a id="indexinfoapply" href="/login.php" class="CustomLink" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block;">Login</a>';
     echo '</div>';
 } else {
     echo '<p>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</p>';
