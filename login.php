d1936f9af143c2879ccfc4bf3f838866a2a23191
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 19:51:46 +1000

    seeing if fieldset will change it

--- login.php
+++ login.php
@@ -6,14 +6,16 @@
 <body>
 <?php include 'nav.inc'; ?>
 
-<form id="form2" action="process_login.php" method="post" style="display: flex; justify-content: center;">
+<form action="process_login.php" method="post" style="display: flex; justify-content: center;">
   <div class="login-form">
+    <fieldset>
     <label for="username">Username:</label>
     <input type="text" name="username" id="username">
-
+    </fieldset>
+    <fieldset>
     <label for="password">Password:</label>
     <input type="password" name="password" id="password">
-
+  </fieldset>
     <input type="submit" value="Login">
   </div>
 </form>
