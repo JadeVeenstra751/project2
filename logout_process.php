a3971ebe0d4f02f2f39d36e82b5ff82b48fa6ea0
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 20:59:32 +1000

    changing logut and adding process php back

---
+++ logout_process.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    // removes all session variables
+    $_SESSION = array();
+    // destroys the session
+    session_destroy();
+
+    // redirects to login page
+    header("Location: login.php");
+    exit();
+    ?>
+<?php
