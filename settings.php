5ae3304666bd7f28362a8abf7ccfd01de5d63c6b
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-23 16:12:56 +1000

    changing and adding comments in settings.php, adding manage, login and process + adding new button to nav

--- settings.php
+++ settings.php
@@ -1,6 +1,15 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pwd = "";
-    $sql_db = "job_db";
+$host = "localhost"; // server hostname
+$user = "root"; //username for SQL db
+$pwd = ""; //password for SQL user
+$sql_db = "job_db"; //name of db
+
+//connection to SQL database
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+//checks if connection is successful
+if (!$conn) {
+    //if failed, display an error message and stop execution
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
