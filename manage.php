bc6b18023564d93ff87a6a0c806e2476a66e80f1
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-24 20:12:14 +1000

    changing the manage page button alignment and background

--- manage.php
+++ manage.php
@@ -4,42 +4,45 @@
 <title>LeafByte Tech Manage EOIs</title>
 <!-- includes nav.inc -->
 <?php include 'nav.inc'; ?>
-<body>
-<h1>EOI Management</h1>
+<body class="manage">
 <form method="post">
-
+    <br><br>
   <fieldset>
     <legend>List All EOIs</legend>
-    <input type="submit" name="list_all" value="List All EOIs"><br><br>
+    <input type="submit" name="list_all" value="List All EOIs">
   </fieldset>
-
+<br><br>
   <fieldset>
     <legend>List EOIs by Job Reference Number</legend>
     <input type="text" name="job_ref_list" placeholder="Job Reference Number">
-    <input type="submit" name="list_by_job" value="List EOIs"><br><br>
+    <input type="submit" name="list_by_job" value="List EOIs">
   </fieldset>
-
+<br><br>
   <fieldset>
     <legend>List EOIs by Applicant Name</legend>
     <input type="text" name="first_name" placeholder="First Name">
     <input type="text" name="last_name" placeholder="Last Name">
-    <input type="submit" name="list_by_applicant" value="List EOIs"><br><br>
+    <br><br>
+    <input type="submit" name="list_by_applicant" value="List EOIs">
   </fieldset>
-
+<br><br>
   <fieldset>
     <legend>Delete EOIs by Job Reference Number</legend>
     <input type="text" name="job_ref_delete" placeholder="Job Reference Number">
-    <input type="submit" name="delete_by_job" value="Delete EOIs"><br><br>
+    <br><br>
+    <input type="submit" name="delete_by_job" value="Delete EOIs">
   </fieldset>
-
+<br><br>
   <fieldset>
     <legend>Change EOI Status</legend>
     <input type="text" name="eoi_id" placeholder="EOI ID">
     <input type="text" name="new_status" placeholder="New Status">
-    <input type="submit" name="update_status" value="Update Status"><br><br>
+    <br><br>
+    <input type="submit" name="update_status" value="Update Status">
   </fieldset>
 
 </form>
+<br>
 <br>
 </form>
 <!--includes nav!-->
