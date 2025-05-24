e6fb19f44eee522f656808f9c069e8c14bc9a764
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-24 17:03:09 +1000

    fixing the about

--- about.php
+++ about.php
@@ -141,11 +141,11 @@
 
             <dt><strong>Will Stevens</strong></dt>
             <dd>Creator of the <a href="apply.html" class="aboutlink"><strong>Apply</strong></a> Page</dd>
-            <dd>Creator of 7 in Part A<dd>
+            <dd>Creator of 3 and 4 in Part A<dd>
 
             <dt><strong>Muhammad Taki</strong></dt>
             <dd>Creator of the <a href="jobs.html" class="aboutlink"><strong>Position Description</strong></a> Page
-            <dd>Creator of 3 and 4 in Part A<dd>
+            <dd>Creator of 7 in Part A<dd>
 
             <dt><strong>Moji Alak</strong></dt>
             <dd>Creator of the <a href="about.html" class="aboutlink"><strong>About</strong></a> Page</dd>
