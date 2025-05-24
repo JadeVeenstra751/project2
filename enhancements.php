2574a134add9c7b2854b7d26364c713bb112dcb4
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-24 14:53:26 +1000

    trying to fix css for enhancements

--- enhancements.php
+++ enhancements.php
@@ -23,15 +23,12 @@ include 'header.inc'; ?>
 <!--includes nav.inc!-->
 <?php include 'nav.inc';
 
-if (!$isLoggedIn) {//if user not logged in then
-    //issues with css so i had to add in the css into the page to get it to work
-    echo '<body style="background-color: #caba9c;">';
-    echo '<div style="text-align:center; margin: 9em 0;">';
-    echo '<a id="indexinfoapply" href="./login.php" class="CustomLink" style="padding:1em 2em; font-size:1.5em; border-radius:50px; display:inline-block;">Login</a>';
+if (!isset($_SESSION['username'])) {
+    echo '<div class="centered-container">';
+    echo '<a id="indexinfoapply" href="./login.php" class="CustomLink">Login</a>';
     echo '</div>';
 } else {
-    echo '<body style="background-color: #caba9c;">';
-     echo '<div style="text-align:center; margin: 2em 0;">';
+    echo '<div class="centered-container logged-in">';
     echo '<h1>Welcome, ' . htmlspecialchars($_SESSION['username']) . '!</h1>';
     echo '<a id="indexinfoapply" href="./logout_process.php" class="CustomLink">Logout</a>';
     if ($isManager) {
@@ -42,9 +39,4 @@ if (!$isLoggedIn) {//if user not logged in then
     }
     echo '</div>';
 }
-
-include 'footer.inc';
 ?>
-
-</body>
-</html>
