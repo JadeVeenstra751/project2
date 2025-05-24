fd839dbf8e0e3479b0ee42b2b4079656ed164779
JadeVeenstra751 <105929618@student.swin.edu.au>
2025-05-24 18:03:04 +1000

    adding comments

--- manageraccount_creation_process.php
+++ manageraccount_creation_process.php
@@ -6,38 +6,41 @@ if (!$conn2) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// get and sanitise form data through post
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
 $role = trim($_POST['role']);
 
-echo "Received role: $role<br>";
-
-if ($role !== 'user' && $role !== 'manager') {
-    die("Invalid role selected.");
-}
-
-// Password validation
+// password validation
 if (!preg_match('/^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/', $password)) {
     die("Password must be at least 8 characters long and include at least one letter, one number, and one special character.");
 }
 
-// Check if username exists
+// check if username exists
 $check_query = "SELECT * FROM user WHERE username = ?";
 $check_stmt = mysqli_prepare($conn2, $check_query);
+//bind user param to prevent sql injections
 mysqli_stmt_bind_param($check_stmt, "s", $username);
+//executes query
 mysqli_stmt_execute($check_stmt);
+//stores result
 mysqli_stmt_store_result($check_stmt);
 
+//if the username already exists then display message and stop
 if (mysqli_stmt_num_rows($check_stmt) > 0) {
     echo "Username already taken.";
     exit;
 }
+//clses statement 
 mysqli_stmt_close($check_stmt);
 
+// hashes the password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+// prepares sql statement to insert new user details  
 $insert_query = "INSERT INTO user (username, password, role) VALUES (?, ?, ?)";
 $insert_stmt = mysqli_prepare($conn2, $insert_query);
+//binds param to sql query 
 mysqli_stmt_bind_param($insert_stmt, "sss", $username, $hashed_password, $role);
 
 //if successful the execute
